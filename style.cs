ba329a41f04dcf59f0c767ec868a729dfc960b18
e005690 <carloshenriquecastrolima@gmail.com>
2026-02-12 13:33:41 -0400

    Deixando o jogo facil

---
+++ style.cs
@@ -0,0 +1,19 @@
+body {
+    font-family: Arial;
+    text-align: center;
+    background: #0f172a;
+    color: white;
+    margin-top: 80px;
+}
+
+input {
+    padding: 8px;
+    width: 80px;
+    margin: 10px;
+}
+
+button {
+    padding: 8px 12px;
+    margin: 5px;
+    cursor: pointer;
+}
